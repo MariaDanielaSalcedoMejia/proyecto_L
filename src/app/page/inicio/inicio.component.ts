1f0b2be3b252753b0b4c070870109ad92ff71b73
DanielaSalcedoMejia <danielasalcedo940@gmail.com>
2024-07-31 13:08:29 -0500

    cambios varios

--- src/app/page/inicio/inicio.component.ts
+++ src/app/page/inicio/inicio.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { FritosComponent } from '../fritos/fritos.component';
 
+
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
